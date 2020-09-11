aca67a710c7fc727204caaebe731d548c72934c1
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-09-10 23:54:34 -0500

    decided to make a react component for eventfocus. will actually get something going when i'm less tired

--- src/components/EventFocus.js
+++ src/components/EventFocus.js
@@ -1,1 +1,9 @@
-/*Todo*/
+import React from "react";
+
+class EventFocus extends React.Component {}
+
+//trying to figure out if i want to use a function stateless component or a stateful component
+//researching what the freaking difference is
+/**
+ * making it a component so i can use state for going to another page
+ */
