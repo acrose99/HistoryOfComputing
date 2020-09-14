752c3a1a2d5b713af65c1c64d843d8ee1a771d1d
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-09-14 15:57:42 -0500

    im trying to add the citation and see if this all works

--- src/components/EventFocus.js
+++ src/components/EventFocus.js
@@ -10,10 +10,27 @@ class EventFocus extends React.Component {
       <div className="showEventWrapper">
         <div className="showEvent">
           <div id="header" className="eventHeader">
-            <h2>frick</h2>
+            <h2 style={{ textAlign: "center" }}>frick</h2>
           </div>
-          <p>Example txt</p>
+          <div id="locationTimeWrapper"></div>
+
+          <div className="eventBody">
+            <p>
+              Example txt "Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum."
+            </p>
+          </div>
+          <div className="citation">
+            <p>citation citation </p>
+          </div>
         </div>
+        {/* ent event div */}
       </div>
     );
   }
