22ae3e28c0c25adf4bfa5182ffc815a793deaee4
Alex <acrane2@luc.edu>
2020-11-10 12:34:39 -0600

    I figured out a way for the menu to automatically change what it looks like based on browser size. I thought it would make things look better if a user is changing browser sizes as they look at the content. Before you had to reload it to get a different looking menu. I'm really proud of this! I want the menu on phones to look different than the menu on the full screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Navbar.css";
 
 
-export default function Navbar() {
-    var windowSize = window.matchMedia("(max-width:700px)");
-    if (windowSize.matches) {
-        return (
-            // if window is smaller
-            <div>
-                <p>booh</p>
-            </div>
-        )
+export default class Navbar extends Component {
+
+    // big = over 700px width. mini = less than 700 width
+    constructor() {
+
+        super();
+        this.state = {
+            windowBig: true,
+        }
     }
-    else {
 
+
+    updateMenuStyle() {
+
+        if (window.innerWidth < 700) {
+            this.setState({ windowBig: false, });
+            // if window is smaller
+            // <div>
+            //     <p>booh</p>
+            // </div>
+
+        }
+        else {
+            this.setState({ windowBig: true, });
+        }
+    }
+
+    componentDidMount() {
+        this.updateMenuStyle();
+        window.addEventListener("resize", this.updateMenuStyle.bind(this));
+    }
+
+    render() {
         return (
-            // if window is bigger
-            <div id="Nav-container">
-                <div id="top-container">
-                    <h2 class="title">The History Of Computing</h2>
-                    <img className="Nav-icon" src={require("../images/artificial-intelligence.png")} alt="" />
-
-                </div>
-                <div id="bottom-container">
-                    <button class="menuItem">The Timeline</button>
-                    <button class="menuItem">The Book</button>
-                    <button class="menuItem">About Us</button>
-                    <button class="menuItem">SSL</button>
-                    <button class="menuItem"><img class="menuImg" src={require("../images/github.svg")} /></button>
-
-                </div>
+            <div>
+                {
+                    this.state.windowBig === true ? <div>BIGGER HELLO</div> : <div>hello</div>
+                }
             </div>
-        )
-    }//end if
+        );
 
+
+    }
 }
+    //     return (
+    //         // if window is bigger
+    //         <div id="Nav-container">
+
+                    //             {/* if(state.mediaSize == true) ? */}
+/* //             <div id="top-container"> *
+                         <h2 class="title">The History Of Computing</h2>
+//                 <img className="Nav-icon" src={require("../images/artificial-intelligence.png")} alt="" />
+
+//             </div>
+//             <div id="bottom-container">
+        //                 <button class="menuItem">The Timeline</button>
+//                 <button class="menuItem">The Book</button>
+//                 <button class="menuItem">About Us</button>
+//                 <button class="menuItem">SSL</button>
+//                 <button class="menuItem"><img class="menuImg" src={require("../images/github.svg")} /></button>
+
+//             </div>
+//         </div>
+//     )
+// }//end if */
+
