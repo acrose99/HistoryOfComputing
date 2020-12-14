b3306a8a8f57caa16b93078f74831f04f93bfbb1
Alexander Rose <acrosemail@gmail.com>
2020-12-14 15:56:31 -0600

    Updates

---
+++ precache-manifest.3057eb2c740b9274e09dc387211cdc5d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5359bb4e87f2643877befe471c44670",
+    "url": "/HistoryOfComputing/home/index.html"
+  },
+  {
+    "revision": "b6f81f783acd785f63c3",
+    "url": "/HistoryOfComputing/home/static/css/2.f62f5b6d.chunk.css"
+  },
+  {
+    "revision": "b6812b14ac1bdae88e7d",
+    "url": "/HistoryOfComputing/home/static/css/main.fb29f0d8.chunk.css"
+  },
+  {
+    "revision": "b6f81f783acd785f63c3",
+    "url": "/HistoryOfComputing/home/static/js/2.5655b4c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HistoryOfComputing/home/static/js/2.5655b4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6812b14ac1bdae88e7d",
+    "url": "/HistoryOfComputing/home/static/js/main.6bda4086.chunk.js"
+  },
+  {
+    "revision": "a79f31ab18e9f1acd80c",
+    "url": "/HistoryOfComputing/home/static/js/runtime-main.a24db1f4.js"
+  },
+  {
+    "revision": "46f70eadbf2e8e492db8de28174baafc",
+    "url": "/HistoryOfComputing/home/static/media/AppleBackdrop.46f70ead.svg"
+  },
+  {
+    "revision": "b1b95ef95e66cbec6f1c5394d5c77bb1",
+    "url": "/HistoryOfComputing/home/static/media/AppleLogo.b1b95ef9.svg"
+  },
+  {
+    "revision": "d68d8b1124b4d39ed9f73e62c11a80a9",
+    "url": "/HistoryOfComputing/home/static/media/Backdrop.d68d8b11.svg"
+  },
+  {
+    "revision": "97c29e99758e716b79120ec4512def2e",
+    "url": "/HistoryOfComputing/home/static/media/FlatLogo.97c29e99.svg"
+  },
+  {
+    "revision": "52e92150cc698ea0ca4281a4cff821cc",
+    "url": "/HistoryOfComputing/home/static/media/Mac128hello.52e92150.png"
+  },
+  {
+    "revision": "4c8c12ecafda538c730987a10e5b8ba1",
+    "url": "/HistoryOfComputing/home/static/media/Mac128world.4c8c12ec.png"
+  },
+  {
+    "revision": "52cf38392811cace3bba7ff518a7b965",
+    "url": "/HistoryOfComputing/home/static/media/alex2.52cf3839.jpeg"
+  },
+  {
+    "revision": "62fc7cf1fd6989be9b57a1dff83c9987",
+    "url": "/HistoryOfComputing/home/static/media/alexC.62fc7cf1.jpg"
+  },
+  {
+    "revision": "6994ca9fdf9e6c6c2b3548d8adfbc852",
+    "url": "/HistoryOfComputing/home/static/media/artificial-intelligence.6994ca9f.png"
+  },
+  {
+    "revision": "55850fd4f9ff330c44033b44ac170fcb",
+    "url": "/HistoryOfComputing/home/static/media/button-left-v.55850fd4.svg"
+  },
+  {
+    "revision": "d2d1b9fad1e5171201be35e35d5b6852",
+    "url": "/HistoryOfComputing/home/static/media/button-right-v.d2d1b9fa.svg"
+  },
+  {
+    "revision": "a5871ad1c2ce650c9025877fe57744f4",
+    "url": "/HistoryOfComputing/home/static/media/ddennis.a5871ad1.jpg"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/HistoryOfComputing/home/static/media/github.5cff78e4.svg"
+  },
+  {
+    "revision": "4841a021644ce0831e535226b02ff439",
+    "url": "/HistoryOfComputing/home/static/media/gkt.4841a021.jpg"
+  },
+  {
+    "revision": "620cd3dbc1b99740af9f88fd7aac044c",
+    "url": "/HistoryOfComputing/home/static/media/jack.620cd3db.png"
+  },
+  {
+    "revision": "a12f105944e056f6e3930d9646c7bd8d",
+    "url": "/HistoryOfComputing/home/static/media/menu.a12f1059.svg"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/HistoryOfComputing/home/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/HistoryOfComputing/home/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/HistoryOfComputing/home/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/HistoryOfComputing/home/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HistoryOfComputing/precache-manifest.946c1b6b53f339ee465186884a830e1a.js"
+  "/HistoryOfComputing/home/precache-manifest.3057eb2c740b9274e09dc387211cdc5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HistoryOfComputing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HistoryOfComputing/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
