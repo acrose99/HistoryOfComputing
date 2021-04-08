45e3b240edf5850e40304ddc07c3baef76be3093
Alex <acrane2@luc.edu>
2021-04-08 15:20:35 -0500

    New foucs event with buttons

--- src/components/FocusEvent.jsx
+++ src/components/FocusEvent.jsx
@@ -5,7 +5,18 @@ export default function FocusEvent(){
     return(
         <div className="showEventWrapper">
             <div className="eventHeader">
+            <div className="vbar-buttons">
+            <button className="vaporwaveBarContents">
+                                <img
+                                    src={require("../images/buttons/media_player_stream_no.png")}
+                                    alt="alt"
+                                />
+            </button>
+            </div>
+            <div>
                 <p id="eventHeader-text">Header Name</p>
+
+            </div>
             </div>
             <div className="eventBody"></div>
         </div>
