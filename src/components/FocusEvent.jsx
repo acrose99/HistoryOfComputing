bebcdd3418ec8a71037bceb147c04638844e8cc6
Alex <acrane2@luc.edu>
2021-04-25 13:44:12 -0500

    resized the focus event divs for all of the media query sizes. still need to fix up the header and some stylistic details. might resize the divs as well

--- src/components/FocusEvent.jsx
+++ src/components/FocusEvent.jsx
@@ -22,12 +22,14 @@ export default function FocusEvent(){
             </div>
             </div>
             <div className="eventBody">
-                <div className="eventBody-image">
+                <div >
                     <img className="eventBody-image" src={require("../images/event-images/EventFocus1.jpg")} alt="event image" />
-                </div>
-                <div className="eventBody-text">
                     <p>The Macintosh 128K, originally released as the Apple Macintosh, is the original Apple Macintosh personal computer. Its beige case consisted of a 9 in (23 cm) CRT monitor and came with a keyboard and mouse. A handle built into the top of the case made it easier for the computer to be lifted and carried. It had an initial selling price of $2,495 (equivalent to $6,140 in 2019). The Macintosh was introduced by the now-famous $370,000 (equivalent to $910,541 in 2019) television commercial directed by Ridley Scott, ‘1984’, that aired on CBS during the third quarter of Super Bowl XVIII on January 22, 1984. Sales of the Macintosh were strong from its initial release on January 24, 1984, and reached 70,000 units on May 3, 1984. Upon the release of its successor, the Macintosh 512K, it was rebranded as the Macintosh 128K. The computer is Model M0001.</p>
+                {/* needs to be positioned that way for the larger sized divs */}
                 </div>
+                {/* <div className="eventBody-text">
+                    <p>The Macintosh 128K, originally released as the Apple Macintosh, is the original Apple Macintosh personal computer. Its beige case consisted of a 9 in (23 cm) CRT monitor and came with a keyboard and mouse. A handle built into the top of the case made it easier for the computer to be lifted and carried. It had an initial selling price of $2,495 (equivalent to $6,140 in 2019). The Macintosh was introduced by the now-famous $370,000 (equivalent to $910,541 in 2019) television commercial directed by Ridley Scott, ‘1984’, that aired on CBS during the third quarter of Super Bowl XVIII on January 22, 1984. Sales of the Macintosh were strong from its initial release on January 24, 1984, and reached 70,000 units on May 3, 1984. Upon the release of its successor, the Macintosh 512K, it was rebranded as the Macintosh 128K. The computer is Model M0001.</p>
+                </div> */}
             </div>
         </div>
     );
