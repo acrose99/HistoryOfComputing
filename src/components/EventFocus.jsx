903132cfbc8d1296d3239f928e653a06abd01c93
Alex <acrane2@luc.edu>
2021-04-25 14:11:11 -0500

    changed focusevent for apple into the design I had made

--- src/components/EventFocus.jsx
+++ src/components/EventFocus.jsx
@@ -142,43 +142,31 @@ class EventFocus extends Component  {
         if (this.props.type === "RetroApple") {
             return (
                 <div className="showEventWrapper">
-                    <div className="showEvent">
-                        <div className="vaporwaveBar">
-                            <div style={RetroAppleStyleBar}  className="vbar-buttons">
-                                <button onClick={this.props.hideEventInFocus} className="vaporwaveBarContents">
-                                    <img
-                                        src={require("../images/buttons/media_player_stream_no.png")}
-                                        alt="alt"
-                                    />
-                                </button>
-                                <button className="vaporwaveBarContents">
-                                    <img
-                                        src={require("../images/buttons/button-left-v.svg")}
-                                        alt="Error"
-                                        className="leftRight"
-                                    />
-                                </button>
-                                <button className="vaporwaveBarContents">
-                                    <img
-                                        src={require("../images/buttons/button-right-v.svg")}
-                                        alt="burr"
-                                    />
-                                </button>
-                            </div>
+                    <div className="eventHeader" style={RetroAppleStyleBar}>
+                        <div className="vbar-buttons" >
+                            <button className="vaporwaveBarContents" onClick={this.props.hideEventInFocus}>
+                            <img
+                                    src={require("../images/buttons/media_player_stream_no.png")}
+                                    alt="Close"
+                                />
+                            </button>
                         </div>
-                        <div style={RetroAppleStyleDiv} id="header" className="eventHeader">
-                            <h2 id="eventHeader-text" style={RetroAppleStyleText}>
-                                {this.props.header}
-                            </h2>
-                            <div id="locationTimeWrapper"/>
+                        <div className="vbar-divs">
+                            <p id="eventHeader-text">Header Name</p>
                         </div>
-                        <div style={RetroAppleStyleDiv} className="eventBody">
-                            {this.renderBody()}
-                        </div>
-                        <div style={RetroAppleStyleDiv} className="event-citation">
-                            {this.renderCitations()}
+                        <div className="vbar-divs">
+
                         </div>
                     </div>
+                <div className="eventBody">
+                    <div>
+                    <img className="eventBody-image" src={require("../images/event-images/EventFocus1.jpg")} alt="event image" />
+                    <p>The Macintosh 128K, originally released as the Apple Macintosh, is the original Apple Macintosh personal computer. Its beige case consisted of a 9 in (23 cm) CRT monitor and came with a keyboard and mouse. A handle built into the top of the case made it easier for the computer to be lifted and carried. It had an initial selling price of $2,495 (equivalent to $6,140 in 2019). The Macintosh was introduced by the now-famous $370,000 (equivalent to $910,541 in 2019) television commercial directed by Ridley Scott, ‘1984’, that aired on CBS during the third quarter of Super Bowl XVIII on January 22, 1984. Sales of the Macintosh were strong from its initial release on January 24, 1984, and reached 70,000 units on May 3, 1984. Upon the release of its successor, the Macintosh 512K, it was rebranded as the Macintosh 128K. The computer is Model M0001.</p>
+
+                    </div>
+                </div>
+
+
                 </div>
             );
         } else if (this.props.type === "CPM") {
