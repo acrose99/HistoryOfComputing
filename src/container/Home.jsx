5ac09249d056ed39a883bbd8ad0484c8a91a0ece
acrose99 <acrosemail@gmail.com>
2021-04-29 11:32:38 -0500

    REF: Mobile Homepage

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,27 +1,46 @@
 import React, {Component} from "react";
 import "./Home.css";
-
-
-//Material-UI imports is placeholder until we create our own, probably in a few days.
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import MacHello from "../images/buttons/Mac128hello.png"
+import MacWorld from "../images/buttons/Mac128world.png"
+import {themes} from '../themeStyles';
+
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.props.toggleTheme('Vaporwave');
+        this.state = {
+            src: MacHello
+        }
+        this.onMacHover = this.onMacHover.bind(this);
+        this.onMacLeave = this.onMacLeave.bind(this);
+    }
+    onMacHover() {
+        this.setState({
+            src: MacWorld
+        });
+    }
+    onMacLeave() {
+        this.setState({
+            src: MacHello
+        });
     }
     render() {
         return (
-            <div>
-                <div className="Home-container">
+            <div className="Home-container">
+                <div style={{background: themes.Vaporwave.background}}>
                     <Navbar/>
-                    <div className="Home-media">
+                    <div className="Home-content">
+                        <img id="macHome" onMouseEnter={() => this.onMacHover} onMouseLeave={this.onMacLeave}  src={this.state.src} alt="MacHello"/>
+                        <h1 style={{color: themes.Vaporwave.headerColor}}>
+                            Welcome to The History of Computing!
+                        </h1>
+                        <p style={{color: themes.Vaporwave.textColor}}>
+                            We recommend you first go to the timeline by using the navigation menu above.
+                        </p>
                     </div>
-                    <h1  className="Home-content">
-                        The History of Computing!
-                    </h1>
+                    <Footer/>
                 </div>
-                <Footer/>
             </div>
         )
     }
