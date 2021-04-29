3c68403be3d0d7fbb493710a0e5c098ccdac5042
Alex <acrane2@luc.edu>
2021-04-29 13:54:16 -0500

    readded the teams iamges to comply with react version 4

--- src/container/Team.jsx
+++ src/container/Team.jsx
@@ -3,6 +3,13 @@ import React, {Component} from "react";
 import Navbar from "../components/Navbar";
 import Teammate from "../components/Teammate";
 import Footer from "../components/Footer";
+// image imports 
+import AlexRoseImg from "../images/team/alex.jpeg";
+import AlexCraneImg from "../images/team/alexC.jpg";
+import jackImg from "../images/team/jack.png";
+import georgeImg from "../images/team/gkt.jpg";
+import dennisImg from "../images/team/ddennis.jpg";
+
 //TODO Change based on context
 class Team extends Component{
     // const theme = useContext(ThemeStyles);
@@ -18,8 +25,10 @@ class Team extends Component{
                     <div className="team-row">
                         <div className="team-column">
                             <Teammate
-                                image={require("../images/team/alex.jpeg").default}
+                                image={AlexRoseImg}
                                 name="Alex Rose"
+                                alt="Alex"
+
                                 role="Lead Programmer"
                                 bio="I'm a Junior studying Computer Science at Loyola. I'm also a Mulcahy Fellow at the Software Systems Laboratory and the VP of the Don't Panic CS Club. I like web development and scripting"
                                 links={[
@@ -33,8 +42,10 @@ class Team extends Component{
                             />
 
                             <Teammate
-                                image={require("../images/team/alexC.jpg").default}
+                                image={AlexCraneImg}
                                 name="Alex Crane"
+                                alt="Alex"
+
                                 role="Programmer"
                                 bio=" I'm Alex Crane and I am a senior Software Engineering major at Loyola. I am an active member of Alpha Phi Omega. I like web development and Object-Oriented programming."
                                 links={[
@@ -43,9 +54,11 @@ class Team extends Component{
                                 ]}
                                 fav="Java"
                             />
-                            <Teammate image={require("../images/team/jack.png").default} name="Jack Narowski" role="Programmer"
+                            <Teammate image={jackImg} name="Jack Narowski" role="Programmer"
                                       bio="Jacob Narowski is a Software Engineering student at Loyola University. He is specifically interested in research, integrated software, and micro-computers."
                                       links={[["Github", "https://github.com/JackNarowski"], ["Personal Website", "https://jacknarowski.com/"]]}
+                                      alt="Jack"
+                                     
                                       fav="React"
                             >
                             </Teammate>
@@ -53,7 +66,8 @@ class Team extends Component{
 
                         <div className="team-column">
                             <Teammate
-                                image={require("../images/team/gkt.jpg").default}
+                                image={georgeImg}
+                                alt="George"
                                 name="George K. Thiruvathukal"
                                 role="Writer (Technical History)"
                                 bio="Hi! I’m George K. Thiruvathukal. I’m professor of computer science at Loyola University Chicago and visiting faculty at Argonne National Laboratory."
@@ -68,7 +82,7 @@ class Team extends Component{
                             />
 
                             <Teammate
-                                image={require("../images/team/ddennis.jpg").default}
+                                image={dennisImg}
                                 name="David Dennis"
                                 role="Writer (Cultural History)"
                                 bio=" David B. Dennis (Ph.D., UCLA, 1991; B.A., University of Wisconsin, 1984) is a Professor of History at Loyola University Chicago where he teaches courses on Western Humanities, Modern European Cultural and Intellectual History, and the Cultural History of Computing."
